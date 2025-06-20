e8138c7ea2d848aa52f5f602ea1d81f2f4cdc043
Lakmini <lakminitg@gmail.com>
2025-06-20 10:31:55 +0530

    third commit

---
+++ lakmi.py
@@ -0,0 +1,11 @@
+num_int1 = 10
+num_int2=12
+
+num_float1 = float(num_int1)
+num_float2 = float(num_int2)
+
+num_float3 =num_float1 + num_float2
+
+
+
+print(f"Sum: {num_float3}, Type: {type(num_float3)}")
